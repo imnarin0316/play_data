de6efa890ed259cd771c768863c3f28781a298a1
imnarin0316 <imnarin0316@gmail.com>
2023-08-16 12:06:35 +0900

    feat: use 컴프리헨션

--- python_basic/forin_if.py
+++ python_basic/forin_if.py
@@ -87,50 +87,34 @@ for su8 in range(1, 10) :
 
 #(9) 다음 리스트가 가진 값에 두배(* 2)를 가지는 새로운 리스트를 만드시오. (리스트 컴프리헨션 이용)
 lst = [10, 30, 70, 5, 120, 700, 1, 35]
-l1 = []
-for su in lst: 
-    l1.append(su*2)
+l1 = [su*2 for su in lst]
 print(l1)
 
 #(10) 다음 리스트가 가진 값에 10배의 값을 가지는 값을 (원래값, 10배값) 의 튜플 묶음으로 가지는 리스트를 만드시오 (리스트 컴프리헨션 이용)
 # Ex) [(10,100), (30,300), .., (35, 350)]
 lst = [10, 30, 70, 5, 5, 120, 700, 1, 35, 35]
-l2 = []
-for su10 in lst:
-    l2.append((su10, su10*10))
+l2 = [(su, su*10) for su in lst]
 print(l2)
 
 
 #(11) 다음 리스트가 가진 값들 중 3의 배수만 가지는 리스트를 만드시오. (리스트 컴프리헨션 이용)
 lst2 = [ 3, 20, 33, 21, 33, 8, 11, 10, 7, 17, 60, 120, 2]
-l3 = []
-for su11 in lst2:
-    if su11 % 3 == 0 :
-        l3.append(su11)
+l3 = [su for su in lst2 if su%3==0]
 print(l3)
 
 #(12) 다음 파일이름들을 담은 리스트에서 확장자가 exe인 파일만 골라서 새로운 리스트에 담으시오.(string의 endswith()함수 이용)
 file_name=["test.txt", "a.exe", "jupyter.bat", "function.exe", "b.exe", "cat.jpg", "dog.png", "run.exe", "i.dll"]
-exe = []
-for file in file_name:
-    if "exe" in file :
-        exe.append(file)
+exe = [f for f in file_name if 'exe' in f]
 print(exe)
 
 
 #(13) 다음 중 10글자 이상인 파일명(확장자포함)만 가지는 리스트를 만드시오.
 file_name=["mystroy.txt", "a.exe", "jupyter.bat", "function.exe", "b.exe", "cat.jpg", "dog.png", "run.exe", "i.dll"]
-len10 = []
-for file in file_name:
-    if len(file) >= 10 : 
-        len10.append(file)
+len10 = [f for f in file_name if len(f) >= 10]
 print(len10)
 
 
 #(14) 다음 리스트에서 소문자만 가지는 새로운 리스트를 만드시오.
 str_list = ["A", "B", "c", "D", "E", "F", "g", "h", "I", "J", "k"]
-lowerli = []
-for low in str_list:
-    if low >= 'a':
-        lowerli.append(low)
+lowerli = [s for s in str_list if s >= 'a']
 print(lowerli)
