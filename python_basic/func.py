f51ae3d961f0fae365faeac276f6a110c86f7ca7
imnarin0316 <imnarin0316@gmail.com>
2023-08-18 10:52:51 +0900

    feat: 함수 연습문제 설명 추가

--- python_basic/func.py
+++ python_basic/func.py
@@ -7,10 +7,15 @@ def gogodan(num):
 print(gogodan(5))
 
 
-
 # 2. 시작 정수, 끝 정수를 받아 그 사이의 모든 정수의 합을 구해서 반환하는 함수를 구현
 # (ex: 1, 20 => 1에서 20 사이의 모든 정수의 합계)
 def hab(num1, num2):
+    """
+    지정한 범위의 정수들의 합계를 계산해서 반환하는 함수
+    Parameter
+        start : int - 범위의 시작 정수
+        설명들..~~
+    """
     su = list(range(num1, num2+1))
     return sum(su)
 
@@ -25,6 +30,21 @@ def hab2(num1=0, num2=10):
 
 print(hab2(num2=3))
 
+
+def accumulate3(**kwargs):
+    # 가변인자를 사용할 경우 docsting에 설명을 꼭 달아줘야함.
+    """
+    Parameter 
+        start : 
+        stop : 
+
+    """
+    start = kwargs['start']
+    stop = kwargs['stop']
+
+    return sum(range(start,stop))
+
+accumulate3(start=10, stop=30)
 
 # 4. 체질량 지수는 비만도를 나타내는 지수로 키가 a미터 이고 몸무게가 b kg일때 b/(a**2) 로 구한다.
 # 체질량 지수가
@@ -50,15 +70,34 @@ def bmi(my_info):
 # print(bmi(input('몸무게, 키를 입력하세요. : ')))
 print(bmi('50, 160'))
 
+# round(float: 반올림) : 소수점 두 자리 이하에서 반올림 됨. 
+# float을 'float: 미터단위' 문자열로 작성해도 무방.
+def check_bmi(tall:float, weight:float) -> str:
+    """
+    bmi 지수를 계산해서 비만도를 알려주는 함수
+    Parameter
+        tall : float - 키, 단위: 미터
+        weight : float - 몸무게. 단위:kg
+    Return
+        str - 비만도(저체중, 정상, 과체중, 비만)
+    Raise
+    """
+
 
 
 # 람다식
 #5. filter()를 이용해 다음 리스트에서 양수만 추출히 리스트를 구현
 ex1 = [1, -10, -2, 20, 3, -5, -7, 21]
-print(list(filter(lambda x: x, ex1)))
+print(list(filter(lambda x: x > 0, ex1)))
 
+print([v for v in ex1 if v>0])
 
 #6. filter()와 map()을 이용해 다음 리스트에서 음수만 추출한 뒤 그 2 제곱한 값들을 가지는 리스트를 구현
 ex2 = [1, -10, -2, 20, 3, -5, -7, 21]
 li = list(filter(lambda x : x < 0, ex2))
 print(list(map(lambda x : x**2 , li)))
+
+# 합쳐서 해도 무방
+print(list(map(lambda x : x**2 , list(filter(lambda x : x < 0, ex2)))))
+
+
