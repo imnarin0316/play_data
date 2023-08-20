4428b3a6d959d0659325fa64ae8118328566af36
imnarin0316 <imnarin0316@gmail.com>
2023-08-20 11:49:02 +0900

    feat: 문자열 뒤집기

--- coding_test/test.py
+++ coding_test/test.py
@@ -1,36 +1,6 @@
-<<<<<<< Updated upstream
-def solution(arr):
-    stk = []
-    i = 0
-    
-    while i < len(arr) : 
-        if stk :
-            if stk[-1] < arr[i]:
-                stk.append(arr[i])
-                i += 1
-                print(stk, i)
-            else: 
-                stk.pop()
-                print(stk, i)
-        else:
-            stk.append(arr[i])
-            print(stk, i)
-            i += 1
-            
-    return stk
-
-print(solution([1, 4, 2, 5, 3]))
-
-=======
-def solution(my_string, queries):
-    answer = my_string
-
-    for a, b in queries:
-        answer = answer.replace(answer[a:b+1], answer[a:b+1][::-1])
-
+def solution(my_string, s, e):
+    answer = my_string.replace(my_string[s:e+1], my_string[e:s-1:-1])
+    # print(my_string[6:13], my_string[-6:-10])
     return answer
 
-
-
-print(solution("rermgorpsam",[[2, 3], [0, 7], [5, 9], [6, 10]]))
->>>>>>> Stashed changes
+print(solution("Progra21Sremm3", 6,12))
