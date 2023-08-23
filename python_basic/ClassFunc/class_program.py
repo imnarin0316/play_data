de215faf543e6aca001213471a8d32cee8a894c5
imnarin0316 <imnarin0316@gmail.com>
2023-08-23 17:44:14 +0900

    feat: class_상단 꾸미기

--- python_basic/ClassFunc/class_program.py
+++ python_basic/ClassFunc/class_program.py
@@ -38,8 +38,33 @@ about.place(x=50, y=112,  width=664, height=50 )
 
 t_name = Label(about, text="이름:", font=("Pretendard",12), bg='#f0f0f0')
 t_name.place(x=26, y=15, width=31 ,height=20 )
-s_name = Entry(about)
-s_name.place(x=57, y=5, width=53, height=40)
+s_name = Entry(about, bd=0)
+s_name.place(x=60, y=5, width=53, height=40)
+
+t_age = Label(about, text="나이:", font=("Pretendard",12), bg='#f0f0f0')
+t_age.place(x=137, y=15, width=31 ,height=20 )
+s_age = Entry(about, bd=0)
+s_age.place(x=170, y=5, width=53, height=40)
+
+t_address = Label(about, text="주소:", font=("Pretendard",12), bg='#f0f0f0')
+t_address.place(x=233, y=15, width=38 ,height=20 )
+s_address = Entry(about, bd=0)
+s_address.place(x=271, y=5, width=54, height=40)
+
+t_lang = Label(about, text="국어:", font=("Pretendard",12), bg='#f0f0f0')
+t_lang.place(x=337, y=15, width=38 ,height=20 )
+s_lang = Entry(about, bd=0)
+s_lang.place(x=375, y=5, width=54, height=40)
+
+t_en = Label(about, text="영어:", font=("Pretendard",12), bg='#f0f0f0')
+t_en.place(x=441, y=15, width=38 ,height=20 )
+s_en = Entry(about, bd=0)
+s_en.place(x=479, y=5, width=54, height=40)
+
+t_math = Label(about, text="수학:", font=("Pretendard",12), bg='#f0f0f0')
+t_math.place(x=545, y=15, width=38 ,height=20 )
+s_math = Entry(about, bd=0)
+s_math.place(x=583, y=5, width=54, height=40)
 
 # 버튼들 프레임
 btns = Frame(root, width=200, height=50, bg='#F0F0F0')
