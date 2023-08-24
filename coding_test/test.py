cc595d67575594e1031020163f3a69e33e97274e
imnarin0316 <imnarin0316@gmail.com>
2023-08-24 19:07:22 +0900

    feat: coding 조건 반복

--- coding_test/test.py
+++ coding_test/test.py
@@ -1,6 +1,23 @@
 def solution(arr):
-    answer = [ su/2 if su >= 50 and su % 2 == 0 else su*2 if  su < 50 and su % 2 != 0 else su for su in arr  ]
-    return answer
+    count = 1
+    list_arr = [arr]
+    
+    while True:
+        li = []
+        for su in list_arr[count-1]:
+            if su >= 50 and su % 2 == 0:
+                li.append(int(su/2))
+            elif su < 50 and su % 2 != 0:
+                li.append(int(su*2 + 1))
+            else:
+                li.append(int(su))
+            
+        list_arr.append(li)
+        count += 1
+        print(list_arr)
+
+        if list_arr[count-1] == list_arr[count-2]:
+            return count-2  
 
 print(solution([1, 2, 3, 100, 99, 98]))
 
