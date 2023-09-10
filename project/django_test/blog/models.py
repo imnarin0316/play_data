ceaa77da215ccc99496ba67222a726b5348b09f2
imnarin0316 <imnarin0316@gmail.com>
2023-09-10 12:57:16 +0900

    feat: django blog 자동으로 시간 추가, 업뎃 기능 추가

--- project/django_test/blog/models.py
+++ project/django_test/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    
+    created_at = models.DateTimeField(auto_now_add=True) #자동으로 시간 추가 auto_now_add
+    udated_at = models.DateTimeField(auto_now=True) # 수정된 시간 자동으로 지금으로 변경
+    #auther 추후 작성예정.
+
+    def __str__(self):
+        return f"[{self.pk}]{self.title}"
