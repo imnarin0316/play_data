cda75f4449eb6321c7935c4d53e1abe4cc086405
imnarin0316 <141836151+imnarin0316@users.noreply.github.com>
2023-09-15 10:35:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,13 @@
+import math
+def solution(price):
+    if price >= 500000:
+        answer = price * 0.8
+    elif price >= 300000:
+        answer = price * 0.9
+    elif price >= 100000:
+        answer = price * 0.95
+    else:
+        answer = price
+    return math.floor(answer)
+    
+        
