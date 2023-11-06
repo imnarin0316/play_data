92974a4483d788cd36f9459ccc290f5c604b01e8
imnarin0316 <imnarin0316@gmail.com>
2023-11-06 22:49:19 +0900

    docs: 주석

--- coding_test/간단한_논리연산.py
+++ coding_test/간단한_논리연산.py
@@ -4,7 +4,7 @@ def solution(x1, x2, x3, x4):
     s2 = True
     # 1식은 True가 하나라도 포함되지 않으면 false
     # 2식은 False가 하나라도 포함되지 않으면 True
-
+# 
     if True not in (x1, x2):
         s1 = False
     if True not in (x3, x4):
